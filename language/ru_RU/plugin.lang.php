e775d1ef51084ab890324433fbb3e299a1864437
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-25 17:45:56 +0000

    [social_buttons] Update ru_RU, thanks to : yuragal
    git-svn-id: http://piwigo.org/svn/extensions/SocialButtons@20396 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/ru_RU/plugin.lang.php
+++ language/ru_RU/plugin.lang.php
@@ -8,4 +8,7 @@ $lang['Toolbar'] = 'На панели инструментов';
 $lang['Annotation'] = 'Примечание';
 $lang['Bubble'] = 'Пузырь';
 $lang['Inline text'] = 'Вписанный текст';
+$lang['Right bubble'] = 'Пузырь справа';
+$lang['Top bubble'] = 'Пузырь сверху';
+$lang['Button type'] = 'Тип кнопки';
 ?>
