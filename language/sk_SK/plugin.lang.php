a13f2f856bf902657cc5bfcb3debfb23ec8a6899
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-01-29 12:08:55 +0000

    [social_buttons] Update sk_SK, thanks to : dodo
    git-svn-id: http://piwigo.org/svn/extensions/SocialButtons@20447 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/sk_SK/plugin.lang.php
+++ language/sk_SK/plugin.lang.php
@@ -7,4 +7,7 @@ $lang['Annotation'] = 'Vysvetlivka';
 $lang['Bubble'] = 'Bublina';
 $lang['Inline text'] = 'Text v riadku';
 $lang['Via'] = 'Cez';
+$lang['Top bubble'] = 'Horná bublina';
+$lang['Right bubble'] = 'Pravá bublina';
+$lang['Button type'] = 'Typ tlačidla';
 ?>
