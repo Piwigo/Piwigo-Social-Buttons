ed2a9eaf2ea45e05932e023278ddeddf3fb7d923
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-04-01 16:48:07 +0000

    [social_buttons] Add zh_TW, thanks to : tedx
    git-svn-id: http://piwigo.org/svn/extensions/SocialButtons@21958 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/zh_TW/plugin.lang.php
@@ -0,0 +1,36 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Annotation'] = '註釋';
+$lang['Bottom'] = '底部';
+$lang['Bubble'] = '氣泡';
+$lang['Button position on picture page'] = '圖片頁面上的按鈕位置';
+$lang['Button type'] = '按鈕類型';
+$lang['Display buttons'] = '顯示按鈕';
+$lang['Inline text'] = '行內文字';
+$lang['Right bubble'] = '右氣泡';
+$lang['Toolbar'] = '工具欄';
+$lang['Top'] = '頂部';
+$lang['Top bubble'] = '頂部氣泡';
+$lang['on photo and album'] = '在圖片和相冊';
+$lang['only on photo'] = '只在圖片上';
+?>
