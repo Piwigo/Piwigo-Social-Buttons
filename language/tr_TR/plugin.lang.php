9c5a7d3ac9cea3331061b6e62fb4e9fbc59adb42
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-05 15:40:52 +0000

    [social_buttons] Update tr_TR, thanks to : LazBoy
    git-svn-id: http://piwigo.org/svn/extensions/SocialButtons@24777 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/tr_TR/plugin.lang.php
+++ language/tr_TR/plugin.lang.php
@@ -14,4 +14,7 @@ $lang['Button type'] = 'Buton tipi';
 $lang['Display buttons'] = 'Butonları göster';
 $lang['on photo and album'] = 'fotoğraf ve albümde';
 $lang['only on photo'] = 'sadece fotoğrafta';
+$lang['Community'] = 'Topluluk';
+$lang['When light mode is activated no external script is loaded, it <b>speeds up the load of the page and disables user tracking</b> but also disables annotations and in-page popups.'] = 'Sade kip aktive edildiğinde dış komut grupları yüklenmez, <b>sayfa yüklenmesini hızlandırır ve kullanıcı izlemeyi devre dışı bırakır</b> fakat aynı zamanda dipnotları ve sayfadaki açılır pencereleri de devre dışı bırakır.';
+$lang['Light mode'] = 'Sade kip';
 ?>
