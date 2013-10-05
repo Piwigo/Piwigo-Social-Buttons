5fa8e7fc2fe424167ad2b3863fd3abf81d3a3f29
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-05 15:40:50 +0000

    [social_buttons] Update ru_RU, thanks to : Konve
    git-svn-id: http://piwigo.org/svn/extensions/SocialButtons@24776 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/ru_RU/plugin.lang.php
+++ language/ru_RU/plugin.lang.php
@@ -14,4 +14,7 @@ $lang['Button type'] = 'Тип кнопки';
 $lang['Display buttons'] = 'Показывать кнопки';
 $lang['on photo and album'] = 'на фотографии и альбоме';
 $lang['only on photo'] = 'только на фотографии';
+$lang['Community'] = 'Общество';
+$lang['Light mode'] = 'Режим Light';
+$lang['When light mode is activated no external script is loaded, it <b>speeds up the load of the page and disables user tracking</b> but also disables annotations and in-page popups.'] = 'Когда включен режим Light, то внешние скрипты не загружаются, страницы грузятся быстрее. Но при этом отключаются и отслеживание действий пользователей, блокируются аннотации и все всплывающие окна и сообщения.';
 ?>
