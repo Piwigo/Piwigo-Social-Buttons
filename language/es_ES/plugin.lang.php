2c25f8dff7f6bef6e7a953fcfa9c0d555c4b2740
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-07 16:19:31 +0000

    [social_buttons] Update es_ES, thanks to : petaqui
    git-svn-id: http://piwigo.org/svn/extensions/SocialButtons@24799 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/es_ES/plugin.lang.php
+++ language/es_ES/plugin.lang.php
@@ -14,4 +14,7 @@ $lang['Top bubble'] = 'Burbuja superior';
 $lang['Display buttons'] = 'Muestra los botones';
 $lang['on photo and album'] = 'en la foto y el álbum';
 $lang['only on photo'] = 'sólo en la foto';
+$lang['Community'] = 'Comunidad';
+$lang['Light mode'] = 'Modo ligero';
+$lang['When light mode is activated no external script is loaded, it <b>speeds up the load of the page and disables user tracking</b> but also disables annotations and in-page popups.'] = 'Cuando el modo ligero está activado no se carga ningún script externo, esto <b>acelera la carga de la página y desactiva  el seguimiento de usuarios</b> pero también desactiva las anotaciones y ventanas emergentes de la página.';
 ?>
