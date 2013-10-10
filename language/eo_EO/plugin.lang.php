97a47e1a7496b1d47438c0c54ae98005cfc6bab5
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-10 04:18:57 +0000

    [social_buttons] Add eo_EO, thanks to : sylvbarrier
    git-svn-id: http://piwigo.org/svn/extensions/SocialButtons@24823 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/eo_EO/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Annotation'] = 'Prinoto';
+$lang['Bottom'] = 'Malsupro';
+$lang['Bubble'] = 'Veziko';
+$lang['Button position on picture page'] = 'Butonopozicio sur la bildopaĝo';
+$lang['Button type'] = 'Butontipo';
+$lang['Community'] = 'Komunumo';
+$lang['Display buttons'] = 'Vidigi butonoj';
+$lang['Inline text'] = 'Enlinia teksto';
+$lang['Light mode'] = 'Maldensa reĝimo';
+$lang['Right bubble'] = 'Dekstra veziko';
+$lang['Toolbar'] = 'Ilobreto';
+$lang['Top'] = 'Supro';
+$lang['Top bubble'] = 'Supra veziko';
+$lang['When light mode is activated no external script is loaded, it <b>speeds up the load of the page and disables user tracking</b> but also disables annotations and in-page popups.'] = 'Kiam la maldensa reĝimo aktiviĝas, neniu ekstera skripto estas ŝutita. Tio <b>plirapidigas la paĝan alŝuton kaj malŝaltas la ŝanĝospurado de la uzanto</b> sed malŝaltas ankaŭ prinotojn kaj enpaĝajn ŝprucfenestrojn.';
+$lang['on photo and album'] = 'sur foto kaj fotaro';
+$lang['only on photo'] = 'nur sur foto';
+?>
