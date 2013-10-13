99426e11bfd32db600b1492adafd48b75a536276
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-13 15:48:11 +0000

    [social_buttons] Update sk_SK, thanks to : dodo
    git-svn-id: http://piwigo.org/svn/extensions/SocialButtons@24884 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/sk_SK/plugin.lang.php
+++ language/sk_SK/plugin.lang.php
@@ -13,4 +13,7 @@ $lang['Button type'] = 'Typ tlačidla';
 $lang['only on photo'] = 'len na fotke';
 $lang['on photo and album'] = 'na fotke a albume';
 $lang['Display buttons'] = 'Zobraziť tlačidlá';
+$lang['When light mode is activated no external script is loaded, it <b>speeds up the load of the page and disables user tracking</b> but also disables annotations and in-page popups.'] = 'Keď sa svetelný režim aktivuje, žiadny externý skript sa nedá nahrať, to <b>urýchľuje načítanie stránky a ruší sledovanie používateľov</b>, ale tiež preruší anotácie a vyskakovanie okien.';
+$lang['Light mode'] = 'Svetelný režim';
+$lang['Community'] = 'Spoločnosť';
 ?>
