893dc67a427eb90737431e7a3141ddbfe828c565
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-10-16 19:01:22 +0000

    [social_buttons] Update nl_NL, thanks to : Kees Hessels
    git-svn-id: http://piwigo.org/svn/extensions/SocialButtons@24942 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/nl_NL/plugin.lang.php
+++ language/nl_NL/plugin.lang.php
@@ -1,17 +1,18 @@
 <?php
-$lang['Button position on picture page'] = 'Knoppositie op de afbeeldingspagina';
+$lang['Button position on picture page'] = 'Knoppositie op de afbeeldingenpagina';
 $lang['Top'] = 'Boven';
 $lang['Bottom'] = 'Beneden';
 $lang['Toolbar'] = 'Werkbalk';
-$lang['Annotation'] = 'Annotatie';
-$lang['Bubble'] = 'Bel';
+$lang['Annotation'] = 'Aantallen';
+$lang['Bubble'] = 'Wolkje';
 $lang['Inline text'] = 'Inline tekst';
-$lang['Shared picture size'] = 'Gedeelde beeldgrootte';
-$lang['Via'] = 'Via';
 $lang['Button type'] = 'Knop model';
-$lang['Right bubble'] = 'Rechter bel';
-$lang['Top bubble'] = 'Bovenste bel';
-$lang['on photo and album'] = ' op foto en album';
-$lang['only on photo'] = ' alleen op de foto';
+$lang['Right bubble'] = 'Wolkje rechts';
+$lang['Top bubble'] = 'Wolkje boven';
+$lang['on photo and album'] = ' bij foto\'s en albums';
+$lang['only on photo'] = ' alleen bij foto\'s';
 $lang['Display buttons'] = 'Toon knoppen';
+$lang['Community'] = 'Gemeenschap';
+$lang['Light mode'] = 'light versie';
+$lang['When light mode is activated no external script is loaded, it <b>speeds up the load of the page and disables user tracking</b> but also disables annotations and in-page popups.'] = 'Wanneer de light versie wordt geactiveerd, wordt er geen extern script geladen. Dit <b>versnelt het laden van de pagina en schakelt het bijhouden van gebruikers-informatie uit</b>, maar schakelt ook het aantallenwolkje en in-pagina popups uit.';
 ?>
