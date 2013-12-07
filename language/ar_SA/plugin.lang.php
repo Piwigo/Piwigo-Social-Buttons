7b3c73958517d4b852edfba7af24c895bdc2aae3
Piwigo-TranslationTeam <translate-github@piwigo.org>
2013-12-07 19:55:14 +0000

    [social_buttons] Update ar_SA, thanks to : aldambi
    git-svn-id: http://piwigo.org/svn/extensions/SocialButtons@25826 68402e56-0260-453c-a942-63ccdbb3a9ee

---
+++ language/ar_SA/plugin.lang.php
@@ -0,0 +1,39 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2013 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['on photo and album'] = 'على الصور والألبوم';
+$lang['only on photo'] = 'على الصور فقط';
+$lang['When light mode is activated no external script is loaded, it <b>speeds up the load of the page and disables user tracking</b> but also disables annotations and in-page popups.'] = 'عند تنشيط وضع الضوء لن يتم تحميل اي برنامج نصي خارجي، فانه <b>يسرع تحميل الصفحة وتعطيل تتبع المستخدم</b> ولكن ايضا تعطل الحواشي واالنوافذ المنبثقة.';
+$lang['Top'] = 'اعلى';
+$lang['Top bubble'] = 'فقاعة علوية';
+$lang['Toolbar'] = 'شريط الأدوات';
+$lang['Right bubble'] = 'فقاعة يمين';
+$lang['Light mode'] = 'وضع الضوء';
+$lang['Inline text'] = 'نص مضمن';
+$lang['Display buttons'] = 'اظهار الازرار';
+$lang['Community'] = 'مجتمع';
+$lang['Button type'] = 'نوع الزر';
+$lang['Button position on picture page'] = 'وضع الازرار على صفحة الصور';
+$lang['Bubble'] = 'فقاعة';
+$lang['Bottom'] = 'اسفل';
+$lang['Annotation'] = 'تعليق توضيحي (حاشية)';
+?>
