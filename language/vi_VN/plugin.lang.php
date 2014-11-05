e49ce63073998e6c5fef36c88f4cc2dc587371a1
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-11-05 06:45:58 +0000

    [social_buttons] Update vi_VN, thanks to thanhphan467
    git-svn-id: http://piwigo.org/svn/extensions/SocialButtons@30367 68402e56-0260-453c-a942-63ccdbb3a9ee

--- language/vi_VN/plugin.lang.php
+++ language/vi_VN/plugin.lang.php
@@ -6,14 +6,12 @@ $lang['Toolbar'] = 'Thanh công cụ';
 $lang['Annotation'] = 'Lời giải thích';
 $lang['Bubble'] = 'Quả cầu';
 $lang['Inline text'] = 'Chữ chèn bên trong';
-$lang['Via'] = 'Thông qua';
 $lang['Button type'] = 'Loại nút';
 $lang['Display buttons'] = 'Hiển thị nút';
 $lang['Right bubble'] = 'Bong bóng bên phải';
 $lang['Top bubble'] = 'Bong bóng bên trên';
-$lang['on photo and album'] = 'trong hình và album';
+$lang['on photo and album'] = 'trong hình và tập ảnh';
 $lang['only on photo'] = 'chỉ trong hình';
 $lang['Community'] = 'Cộng đồng';
 $lang['Light mode'] = 'Chế độ tinh giản';
-$lang['When light mode is activated no external script is loaded, it <b>speeds up the load of the page and disables user tracking</b> but also disables annotations and in-page popups.'] = 'Chế độ tinh giản khi được kích hoạt sẽ không tải bất cứ script nào bên ngoài, làm <b>tăng tốc độ tải trang và vô hiệu hóa việc truy vết người dùng</b> nhưng cũng đồng thời vô hiệu hóa các ghi chú và popup cục bộ.';
-?>
+$lang['When light mode is activated no external script is loaded, it <b>speeds up the load of the page and disables user tracking</b> but also disables annotations and in-page popups.'] = 'Chế độ tinh giản khi được kích hoạt sẽ không tải bất cứ đoạn mã nào bên ngoài, làm <b>tăng tốc độ tải trang và vô hiệu hóa việc truy vết người dùng</b> nhưng cũng đồng thời vô hiệu hóa các ghi chú và cửa sổ bật lên cục bộ.';
