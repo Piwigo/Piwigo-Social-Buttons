5f49f1baf7ff313ba08af2908428a01c279c98ec
Piwigo-TranslationTeam <translate-github@piwigo.org>
2014-12-22 16:55:44 +0100

    Update sr_RS, thanks to knjigor

---
+++ language/sr_RS/plugin.lang.php
@@ -0,0 +1,38 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | Piwigo - a PHP based photo gallery                                    |
+// +-----------------------------------------------------------------------+
+// | Copyright(C) 2008-2014 Piwigo Team                  http://piwigo.org |
+// | Copyright(C) 2003-2008 PhpWebGallery Team    http://phpwebgallery.net |
+// | Copyright(C) 2002-2003 Pierrick LE GALL   http://le-gall.net/pierrick |
+// +-----------------------------------------------------------------------+
+// | This program is free software; you can redistribute it and/or modify  |
+// | it under the terms of the GNU General Public License as published by  |
+// | the Free Software Foundation                                          |
+// |                                                                       |
+// | This program is distributed in the hope that it will be useful, but   |
+// | WITHOUT ANY WARRANTY; without even the implied warranty of            |
+// | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU      |
+// | General Public License for more details.                              |
+// |                                                                       |
+// | You should have received a copy of the GNU General Public License     |
+// | along with this program; if not, write to the Free Software           |
+// | Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, |
+// | USA.                                                                  |
+// +-----------------------------------------------------------------------+
+$lang['Annotation'] = 'Напомена';
+$lang['Inline text'] = 'Инлајн текст';
+$lang['When light mode is activated no external script is loaded, it <b>speeds up the load of the page and disables user tracking</b> but also disables annotations and in-page popups.'] = 'Када је једноставан мод активиран додатне скрипте су искључене, <b>странице се брже учитавају, а искључено је и праћење корисника</b> али су такође искључене и напомене и попап обавештења на страницама.';
+$lang['Light mode'] = 'Једноставан мод';
+$lang['Bottom'] = 'Дно';
+$lang['Bubble'] = 'Балон';
+$lang['Button position on picture page'] = 'Позиција дугмета на страницама са сликом';
+$lang['Button type'] = 'ТИп дугмета';
+$lang['Community'] = 'Заједница';
+$lang['Display buttons'] = 'Прикажи дугме';
+$lang['Right bubble'] = 'Десни балон';
+$lang['Toolbar'] = 'Трака са алаткама';
+$lang['Top'] = 'Врх';
+$lang['Top bubble'] = 'Дугме на врху';
+$lang['on photo and album'] = 'на фотографији и албуму';
+$lang['only on photo'] = 'само на фотографији';
