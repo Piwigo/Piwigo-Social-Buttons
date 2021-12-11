39abafa55d6155e82787d44d5f9463612bc6a16f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2021-12-11 20:37:22 +0100

    Update sl_SI, thanks to maxima

---
+++ language/sl_SI/plugin.lang.php
@@ -0,0 +1,23 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['When light mode is activated no external script is loaded, it <b>speeds up the load of the page and disables user tracking</b> but also disables annotations and in-page popups.'] = 'Ko je aktiviran svetlobni način, se ne naloži nobena zunanja skripta, <b>poveča nalaganje strani in onemogoči sledenje uporabniku</b>, vendar onemogoči tudi opombe in pojavna okna na strani.';
+$lang['Light mode'] = 'Svetlobni način';
+$lang['Community'] = 'Skupnost';
+$lang['only on photo'] = 'samo na sliki';
+$lang['on photo and album'] = 'na sliki in albumu';
+$lang['Display buttons'] = 'Prikazovalni gumbi';
+$lang['Top bubble'] = 'Zgornji mehurček';
+$lang['Right bubble'] = 'Desni mehurček';
+$lang['Button type'] = 'Vrsta gumba';
+$lang['Inline text'] = 'Vgrajeno besedilo';
+$lang['Bubble'] = 'Mehurček';
+$lang['Annotation'] = 'Opomba';
+$lang['Toolbar'] = 'Orodna vrstica';
+$lang['Bottom'] = 'Dno';
+$lang['Top'] = 'Vrh';
+$lang['Button position on picture page'] = 'Položaj gumbov na strani s sliko';
