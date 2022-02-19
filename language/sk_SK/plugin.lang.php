882b952cab6431181db60e075376ef2bb0217d0f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-02-19 06:31:56 +0100

    Update sk_SK, thanks to jose1711

--- language/sk_SK/plugin.lang.php
+++ language/sk_SK/plugin.lang.php
@@ -1,19 +1,17 @@
 <?php
-$lang['Button position on picture page'] = 'Pozícia tlačítka na stránke s fotografiou';
+$lang['Button position on picture page'] = 'Pozícia tlačidiel na stránke s fotografiou';
 $lang['Top'] = 'Hore';
 $lang['Bottom'] = 'Dole';
 $lang['Toolbar'] = 'Panel nástrojov';
 $lang['Annotation'] = 'Vysvetlivka';
 $lang['Bubble'] = 'Bublina';
 $lang['Inline text'] = 'Text v riadku';
-$lang['Via'] = 'Cez';
 $lang['Top bubble'] = 'Horná bublina';
 $lang['Right bubble'] = 'Pravá bublina';
 $lang['Button type'] = 'Typ tlačidla';
 $lang['only on photo'] = 'len na fotke';
 $lang['on photo and album'] = 'na fotke a albume';
 $lang['Display buttons'] = 'Zobraziť tlačidlá';
-$lang['When light mode is activated no external script is loaded, it <b>speeds up the load of the page and disables user tracking</b> but also disables annotations and in-page popups.'] = 'Keď sa svetelný režim aktivuje, žiadny externý skript sa nedá nahrať, to <b>urýchľuje načítanie stránky a ruší sledovanie používateľov</b>, ale tiež preruší anotácie a vyskakovanie okien.';
-$lang['Light mode'] = 'Svetelný režim';
+$lang['When light mode is activated no external script is loaded, it <b>speeds up the load of the page and disables user tracking</b> but also disables annotations and in-page popups.'] = 'Pri zapnutí odľahčeného režimu sa nenačítava žiaden externý skript, to <b>urýchľuje načítanie stránky a deaktivuje sledovanie používateľov</b>, ale tiež vypína anotácie a vyskakovacie okná.';
+$lang['Light mode'] = 'Odľahčený režim';
 $lang['Community'] = 'Spoločnosť';
-?>
