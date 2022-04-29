09453fdd6154cdcd5b99b14344d14ce7a939d613
Piwigo-TranslationTeam <translate-github@piwigo.org>
2022-04-29 06:58:02 +0200

    Update is_IS, thanks to gudjonsi

---
+++ language/is_IS/plugin.lang.php
@@ -0,0 +1,23 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Annotation'] = 'Skýring';
+$lang['Bottom'] = 'Neðst';
+$lang['Bubble'] = 'Bóla';
+$lang['Button position on picture page'] = 'Staða hnappa á myndasíðu';
+$lang['Button type'] = 'Hnappa gerð';
+$lang['Community'] = 'Samfélag';
+$lang['Display buttons'] = 'Sýna hnappa';
+$lang['Inline text'] = 'Innbyggður texti';
+$lang['Light mode'] = 'Ljóshamur';
+$lang['Right bubble'] = 'Hægri bóla';
+$lang['Toolbar'] = 'Tækjastikan';
+$lang['Top'] = 'Efst';
+$lang['Top bubble'] = 'Efsta bóla';
+$lang['When light mode is activated no external script is loaded, it <b>speeds up the load of the page and disables user tracking</b> but also disables annotations and in-page popups.'] = 'Þegar ljóshamur er virkjaður er engin utanaðkomandi forskrift hlaðin, það <b>hraðar hleðslu síðunnar og slekkur á notendarakningu</b> en slekkur einnig á athugasemdum og sprettiglugga á síðu.';
+$lang['on photo and album'] = 'á mynd og albúmi';
+$lang['only on photo'] = 'aðeins á mynd';
