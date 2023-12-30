ca1c6679cf4057dca7452846ec9f433f17fa2f2f
Piwigo-TranslationTeam <translate-github@piwigo.org>
2023-12-30 18:53:44 +0100

    Update is_IS, thanks to gudjonsi

--- language/is_IS/plugin.lang.php
+++ language/is_IS/plugin.lang.php
@@ -21,3 +21,7 @@ $lang['Top bubble'] = 'Efsta bóla';
 $lang['When light mode is activated no external script is loaded, it <b>speeds up the load of the page and disables user tracking</b> but also disables annotations and in-page popups.'] = 'Þegar ljóshamur er virkjaður er engin utanaðkomandi forskrift hlaðin, það <b>hraðar hleðslu síðunnar og slekkur á notendarakningu</b> en slekkur einnig á athugasemdum og sprettiglugga á síðu.';
 $lang['on photo and album'] = 'á mynd og albúmi';
 $lang['only on photo'] = 'aðeins á mynd';
+$lang['Disable'] = 'Slökkva';
+$lang['Enable'] = 'Virkja';
+$lang['Shared picture size'] = 'Deild myndastærð';
+$lang['Via'] = 'Gegnum';
