144e1db39073afd98aceef51a57dd1e3f0ddbaf8
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-01-22 05:22:25 +0100

    Update fr_CA, thanks to bgoutier

---
+++ language/fr_CA/plugin.lang.php
@@ -0,0 +1,8 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['When light mode is activated no external script is loaded, it <b>speeds up the load of the page and disables user tracking</b> but also disables annotations and in-page popups.'] = 'Quand le mode léger est activé aucun script externe n\'est chargé, cela <b>accélère le chargement de la page et désactive le suivi des utilisateurs</b> mais désactive aussi les annotations et les popups détaillés.';
