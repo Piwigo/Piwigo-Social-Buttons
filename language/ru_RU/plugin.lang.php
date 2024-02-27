00bc1abf8f01818429ff6a16ba7f757d6154b032
Piwigo-TranslationTeam <translate-github@piwigo.org>
2024-02-27 07:37:50 +0100

    Update ru_RU, thanks to nadusha

--- language/ru_RU/plugin.lang.php
+++ language/ru_RU/plugin.lang.php
@@ -17,4 +17,7 @@ $lang['only on photo'] = 'только на фотографии';
 $lang['Community'] = 'Общество';
 $lang['Light mode'] = 'Режим Light';
 $lang['When light mode is activated no external script is loaded, it <b>speeds up the load of the page and disables user tracking</b> but also disables annotations and in-page popups.'] = 'Когда включен режим Light, то внешние скрипты не загружаются, страницы грузятся быстрее. Но при этом отключаются и отслеживание действий пользователей, блокируются аннотации и все всплывающие окна и сообщения.';
-?>
+$lang['Disable'] = 'Отключить';
+$lang['Enable'] = 'Включить';
+$lang['Shared picture size'] = 'Размер общего изображения';
+$lang['Via'] = 'Через';
