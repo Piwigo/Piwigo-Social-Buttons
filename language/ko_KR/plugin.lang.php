1f468f774bd3e95a322d7b9d409c93711a23c014
Piwigo-TranslationTeam <translate-github@piwigo.org>
2026-02-13 13:21:40 +0100

    Update ko_KR, thanks to yjchoi

---
+++ language/ko_KR/plugin.lang.php
@@ -0,0 +1,27 @@
+<?php
+// +-----------------------------------------------------------------------+
+// | This file is part of Piwigo.                                          |
+// |                                                                       |
+// | For copyright and license information, please view the COPYING.txt    |
+// | file that was distributed with this source code.                      |
+// +-----------------------------------------------------------------------+
+$lang['Annotation'] = '주석';
+$lang['Bottom'] = '하단';
+$lang['Bubble'] = '버블';
+$lang['Button position on picture page'] = '사진 페이지 내 버튼 위치';
+$lang['Button type'] = '버튼 유형';
+$lang['Community'] = '커뮤니티';
+$lang['Disable'] = '비활성화';
+$lang['Display buttons'] = '버튼 표시';
+$lang['Enable'] = '활성화';
+$lang['Inline text'] = '인라인 텍스트';
+$lang['Light mode'] = '라이트 모드';
+$lang['Right bubble'] = '오른쪽 버블';
+$lang['Shared picture size'] = '공유 사진 크기';
+$lang['Toolbar'] = '도구 모음';
+$lang['Top'] = '상단';
+$lang['Top bubble'] = '상단 버블';
+$lang['Via'] = '경유';
+$lang['When light mode is activated no external script is loaded, it <b>speeds up the load of the page and disables user tracking</b> but also disables annotations and in-page popups.'] = '라이트 모드가 활성화되면 외부 스크립트가 로드되지 않아 <b>페이지 로드 속도가 빨라지고 사용자 추적이 비활성화됩니다</b>. 그러나 주석 및 페이지 내 팝업도 비활성화됩니다.';
+$lang['on photo and album'] = '사진 및 앨범에서';
+$lang['only on photo'] = '사진에서만';
